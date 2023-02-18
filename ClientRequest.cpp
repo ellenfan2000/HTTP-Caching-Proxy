e639607f73b9d291102b102c5a6223bb43c82ad5
unknown <{ks713@duke.edu}>
2023-02-17 19:52:51 -0500

    m

--- ClientRequest.cpp
+++ ClientRequest.cpp
@@ -1,4 +1,40 @@
+#include <iostream>
 #include "ClientRequest.hpp"
+void ClientRequest::parseFirstLine(std::string firstLine){
+std::string sum="";
+int cnt=0;
+for(int i=0;i<firstLine.length();i++){
+	if(firstLine.substr(i,1)!=" "){
+		sum+=firstLine.substr(i,1);
+	}else if(cnt==0){
+		this->request_method=sum;
+		sum="";
+	}else if(cnt==1){
+		this->request_url=sum;
+		sum="";
+	}else if(cnt==2){
+		this->http_version=sum;
+		sum="";
+	}
+}
+}
+void ClientRequest::parseHeader(std::string header){
+	int host_pos=header.find("Host:");
+	if(host_pos==std::string::npos){
+		std::cout<<"No host info!";
+		return;
+	}
+	int host_info_end_pos=header.find_first_of("\r\n");
+	std::string host_info=header.substr(host_pos+6,host_info_end_pos-host_pos);
+	int before_port=host_info.find_first_of(":");
+	if(before_port==std::string::npos){
+		this->host_name=host_info.substr(0,before_port);
+		this->port="80";
+	}else{
+		this->host_name=host_info.substr(0,before_port);
+		this->port=host_info.substr(before_port+1,host_info.length()-this->host_name.length()-1);
+	}
+}
 int main(){
-return 0;
+	return 0;
 }
--- ClientRequest.hpp
+++ ClientRequest.hpp
@@ -1,5 +1,4 @@
-#include<stdio.h> 
-#include<stdlib.h>
+
 #include<string>
 #ifndef ClientRequest_hpp
 #define ClientRequest_hpp
@@ -11,6 +10,12 @@ std::string host_name;
 std::string port;
 public:
  ClientRequest(std::string request_info){
+ 	int request_line_end=request_info.find_first_of("\r\n");
+ 	std::string first_line=request_info.substr(0,request_line_end);
+ 	parseFirstLine(first_line);
+ 	parseHeader(request_info.substr(request_line_end+1,request_info.length()-first_line.length()+2));
  }
+ void parseFirstLine(std::string firstLine);
+ void parseHeader(std::string header);
 };
 #endif

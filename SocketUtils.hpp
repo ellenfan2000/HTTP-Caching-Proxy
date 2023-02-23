ec4f0f96e944f898ffa6a79f70c56906d6c7a4fb
Ellen Fan <rf96@duke.edu>
2023-02-23 10:21:23 -0500

    try boost

--- SocketUtils.hpp
+++ SocketUtils.hpp
@@ -2,6 +2,7 @@
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio.hpp>
 #include <boost/config.hpp>
 #include <cstdlib>
 #include <iostream>
@@ -28,5 +29,52 @@ beast::tcp_stream * connectToServer(const char * host, const char * port){
     // Make the connection on the IP address we get from a lookup
     (*stream).connect(results);
 
+
     return stream;
 };
+
+tcp::socket * connectToServer_socket (const char * host, const char * port){
+    // net::io_context ioc;
+
+    // // These objects perform our I/O
+    // tcp::resolver resolver(ioc);
+    // beast::tcp_stream * stream = new beast::tcp_stream(ioc);
+    // tcp::resolver::query query(host,port);
+
+    // // Look up the domain name
+    // auto const results = resolver.resolve(query);
+
+    // // Make the connection on the IP address we get from a lookup
+    // (*stream).connect(results);
+
+
+    net::io_context io_context;
+
+    // These objects perform our I/O
+    tcp::resolver resolver(io_context);
+
+    tcp::socket * socket = new tcp::socket(io_context);
+    // beast::tcp_stream * stream = new beast::tcp_stream(io_context);
+    tcp::resolver::query query(host,port);
+
+    // Look up the domain name
+    auto const results = resolver.resolve(query);
+
+    // Make the connection on the IP address we get from a lookup
+    net::connect(*socket, results);
+    // (*socket).connect(results);
+
+
+    return socket;
+};
+
+ // if (socket_server->available() > 0){
+            //     std::cout << "num is" << num <<std::endl;
+            //     beast::http::read(*socket_server, buffer2, response);
+            //     http::write(*socket, response);
+            // }
+            // if (socket->available() > 0){
+            //     std::cout << "num is" << num <<std::endl;
+            //     beast::http::read(*socket, buffer2, request2);
+            //     http::write(*socket_server, request2);
+            // }
--- proxy_try.cpp
+++ proxy_try.cpp
@@ -1,19 +1,35 @@
 #include "Cache_try.hpp"
-#include <pthread.h>
+#include <boost/bind/bind.hpp>
+// #include <boost/asio/ssl.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio.hpp>
+#include <boost/config.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
 
+namespace beast = boost::beast;         
+namespace http = beast::http;           
+namespace net = boost::asio;            
+using tcp = boost::asio::ip::tcp;      
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 class Proxy{
 private:
     const char * host;
     const char * port;
-
+    boost::asio::io_context io_context;
     Cache c;
 public:
     Proxy(std::string p):host(NULL), port(p.c_str()){}
 
     void run(){
-        boost::asio::io_context io_context;
+        // boost::asio::io_context io_context;
         tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), strtol(port, NULL, 0)));
         while (true){
             tcp::socket * socket = new tcp::socket(io_context);
@@ -23,6 +39,24 @@ public:
             t.detach();
         }
     }
+    
+    tcp::socket * connectToServer_socket (const char * host, const char * port){
+
+        // These objects perform our I/O
+        tcp::resolver resolver(io_context);
+
+        tcp::socket * socket = new tcp::socket(io_context);
+        // beast::tcp_stream * stream = new beast::tcp_stream(io_context);
+        tcp::resolver::query query(host,port);
+
+        // Look up the domain name
+        auto const results = resolver.resolve(query);
+
+        // Make the connection on the IP address we get from a lookup
+        net::connect(*socket, results);
+        // (*socket).connect(results);
+    return socket;
+};
 
     void requestProcess(tcp::socket * socket){
         beast::flat_buffer buffer;
@@ -45,29 +79,52 @@ public:
             std::cerr<<"Bad Request Type!!!"<<std::endl;
 			exit(1);
         }
+        socket->close();
         delete socket;
-
     }
 
-    bool ifHTTPS(std::string & hostname){
-        if(hostname.find(":") != std::string::npos){
+    bool isHTTPS(std::string hostname, std::string * host, std::string * port){
+        size_t f = hostname.find(":");
+        if(f != std::string::npos){
+            *host = hostname.substr(0, f);
+            *port = hostname.substr(f+1);
             return true;
         }
+        *host = hostname;
+        *port = "80";
         return false;
     }
 
     void post(http::request<http::dynamic_body> * request, tcp::socket * socket){
         // Send the HTTP request to requested server HTTP not HTTPS
-        beast::tcp_stream * stream = connectToServer(std::string(request->at("HOST")).c_str(), "80");
-        http::write(*stream, *request);
+        // beast::tcp_stream * stream = connectToServer(std::string(request->at("HOST")).c_str(), "80");
+        // http::write(*stream, *request);
+
+        // //recieve the HTTP response from the server
+        // boost::beast::flat_buffer buffer2;
+        // http::response<http::dynamic_body> response;
+        // boost::beast::http::read(*stream, buffer2, response);
+
+        // // Send the response to the client
+        // http::write(*socket, response);
+        std::string port;
+        std::string host;
+        isHTTPS(std::string(request->at("HOST")), &host, &port);
+        tcp::socket * socket_server = connectToServer_socket(host.c_str(), port.c_str());
+        http::write(*socket_server, *request);
 
         //recieve the HTTP response from the server
         boost::beast::flat_buffer buffer2;
         http::response<http::dynamic_body> response;
-        boost::beast::http::read(*stream, buffer2, response);
+        
+        boost::beast::http::read(*socket_server, buffer2, response);
+        // std::cout << "reponse header is " << response.base()<<std::endl;
+        // // std::cout << "reponse method is " << response.at()<<std::endl;
+        // std::cout << "reponse status is "<<response.result_int()<<std::endl;
 
         // Send the response to the client
         http::write(*socket, response);
+        socket_server->close();
     }
     void get(http::request<http::dynamic_body> * request, tcp::socket * socket){
         post(request, socket);
@@ -84,7 +141,82 @@ public:
     }
 
     void connect(http::request<http::dynamic_body> * request, tcp::socket * socket){
-        post(request, socket);
+        // post(request, socket);
+        std::string port;
+        std::string host;
+    
+        isHTTPS(std::string(request->at("HOST")), &host, &port);
+        tcp::socket * socket_server = connectToServer_socket(host.c_str(), port.c_str());
+
+        http::write(*socket_server, *request);
+
+        std::string message = "HTTP/1.1 200 OK\r\n\r\n";
+        net::write(*socket, net::buffer(message));
+
+        // net::io_context io_context;
+        // tcp::resolver resolver(io_context);
+        // auto endpoints = resolver.resolve(host, port);
+
+        // boost::asio::io_context my_context;
+        // boost::asio::ssl::context ctx(boost::asio::ssl::context::sslv23);
+
+        // net::ssl::stream<tcp::socket> sockssl(my_context, ctx);
+        // net::connect(sockssl.lowest_layer(),endpoints);
+        // // ctx.set_default_verify_paths();
+        
+        // http::write(sockssl.next_layer(),*request);
+        // beast::flat_buffer buffer;
+        // http::response<http::dynamic_body> response;
+
+
+        // http::read(sockssl, buffer, response);
+        // std::cout << "reponse status is "<<response.result_int()<<std::endl;
+        // if(response.result_int() == 200){
+        //     std::cout << "reponse header is " << response.base()<<std::endl;
+        // }
+
+
+
+        // beast::flat_buffer buffer;
+        // http::response<http::dynamic_body> response;
+
+        // http::read(*socket_server, buffer, response);
+        // std::cout << "reponse status is "<<response.result_int()<<std::endl;
+        // if(response.result_int() == 200){
+        //     std::cout << "reponse header is " << response.base()<<std::endl;
+        // }
+
+
+        int num = 0;
+        std::cout<<"hostname is " <<host <<" portnumber is " <<port <<std::endl;
+        while(true){
+            beast::flat_buffer buffer;
+            http::response<http::dynamic_body> response;
+            http::request<http::dynamic_body> request2;
+            std::cout<<num<<std::endl;
+            socket_server->async_wait(tcp::socket::wait_read, boost::bind(wait_handler, boost::asio::placeholders::error,socket, socket_server,1));
+            socket->async_wait(tcp::socket::wait_read,boost::bind(wait_handler, boost::asio::placeholders::error,socket, socket_server,-1));
+            if(!socket->is_open() || !socket_server->is_open()){
+                break;
+            }
+            num++;
+        }
+        socket_server->close();
+    }
+
+    static void wait_handler(const boost::system::error_code & ec, tcp::socket * socketClient, tcp::socket * socketServer, int flag){
+        beast::flat_buffer buffer;
+        http::response<http::dynamic_body> response;
+        http::request<http::dynamic_body> request;
+        std::cout<<"?" <<std::endl;
+        if(flag == 1){
+            beast::http::read(*socketServer, buffer, response);
+            http::write(*socketClient, response);
+        }
+        else{
+            beast::http::read(*socketClient, buffer, request);
+            http::write(*socketServer, request);
+        }
     }
 };
 

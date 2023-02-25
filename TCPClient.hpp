2de9ee68fb088920694ee005f7b64230e216cc2f
Ellen Fan <rf96@duke.edu>
2023-02-25 03:25:07 -0500

    test needed

--- TCPClient.hpp
+++
@@ -1,8 +0,0 @@
-#include<sys/socket.h>
-#include "netdb.h"
-#include <unistd.h>
-#include <iostream>
-#ifndef TCPClient_H
-#define TCPClient_H
-int client_init(std::string host,std::string port);
-#endif
